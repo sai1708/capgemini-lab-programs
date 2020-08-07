92e9c312249f4c5fb380978ae6bf8265d6fee9db
MONAGARI SAI <MONAGARI SAI@LAPTOP-ME982STI>
2020-08-07 20:47:30 +0530

    movie-eureka

---
+++ movie-eureka-server/src/test/java/com/capg/demo/eurekaserver/MovieEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.demo.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
